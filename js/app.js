f9e8b5487fa99436f8d62a5e873112b08225d591
Cynthia O'Donnell <mimibambino@gmail.com>
2014-11-12 19:26:26 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,252 @@
+var enemyPosY = [60, 143, 226];
+var enemySpeed = [70, 100, 160, 200];
+var gemPosX = [0, 101, 202, 303, 404, 505, 606, 707, 808];
+var gemImages = ['images/gemOrange.png', 'images/gemBlue.png', 'images/gemGreen.png'];
+
+// Randomly choose an element from an array.
+// Shamelessly borrowed from http://bahalps.github.io/frontend-nanodegree-arcade-game/
+var choice = function (array) {
+    return array[Math.floor(Math.random() * array.length)];
+};
+
+var Game = function(){
+  
+}
+
+// Enemies to avoid.
+var Enemy = function() {
+  this.sprite = 'images/enemy-bug.png';
+  this.x = -100;
+  this.y = choice(enemyPosY);
+  this.speed = choice(enemySpeed);
+}
+// Update enemy's position.
+Enemy.prototype.update = function(dt) {
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x = this.x + (this.speed * dt);
+  if (this.x > 960) {
+    this.x = -100;
+    this.y = this.y + 83;
+    this.speed = choice(enemySpeed);
+    if (this.y > 226) {
+      this.y = 60;
+    }
+  }
+
+  if (this.x > -50 && this.x < 50) {
+    this.tileX = 0;
+  } else if (this.x > 50 && this.x < 150) {
+    this.tileX = 101;
+  } else if (this.x > 150 && this.x < 250) {
+    this.tileX = 202;
+  } else if (this.x > 250 && this.x < 350) {
+    this.tileX = 303;
+  } else if (this.x > 350 && this.x < 450) {
+    this.tileX = 404;
+  } else if (this.x > 450 && this.x < 550) {
+    this.tileX = 505;
+  } else if (this.x > 550 && this.x < 650) {
+    this.tileX = 606;
+  } else if (this.x > 650 && this.x < 750) {
+    this.tileX = 707;
+  } else if (this.x > 750 && this.x < 850) {
+    this.tileX = 808;
+  } else if (this.x > 850) {
+    this.tileX = 1;
+  }
+
+  if (player.x === this.tileX && player.y === this.y) {
+    player.reset();
+    gameLife.decrease();
+  }
+}
+
+Enemy.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Player class
+var Player = function() {
+  this.pImg = 'images/char-princess-girl.png';
+  this.x = 404;
+  this.y = 392;
+}
+
+Player.prototype.update = function() {
+  if (this.ctlKey === 'left' && this.x != 0) {
+    this.x = this.x - 101;
+  } else if (this.ctlKey === 'right' && this.x != 808) {
+    this.x = this.x + 101;
+  } else if (this.ctlKey === 'up') {
+    this.y = this.y - 83;
+  } else if (this.ctlKey === 'down' && this.y != 392) {
+    this.y = this.y + 83;
+  }
+  this.ctlKey = null;
+
+  if (this.y < 60) {
+    this.reset();
+    gameLife.decrease();
+  }
+}
+
+Player.prototype.reset = function() {
+  this.x = 404;
+  this.y = 392;
+}
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.pImg), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(key) {
+  this.ctlKey = key;
+}
+
+// Choose gem and place on canvas.
+var Gem = function() {
+  this.gemImg = choice(gemImages);
+  this.x = choice(gemPosX);
+  this.y = choice(enemyPosY);
+  this.count = 0;
+}
+
+// Update gem position when player touches it.
+Gem.prototype.update = function() {
+  if (player.x === this.x && player.y === this.y) {
+    this.gemImg = choice(gemImages);
+    this.x = choice(gemPosX);
+    this.y = choice(enemyPosY);
+    this.count += 1;
+    if (this.count === 1){
+      var scoreHTML = "<p class='num'>%score% Gem collected!</p>";
+      var formattedScore = scoreHTML.replace("%score%", this.count);
+    } else {
+      var scoreHTML = "<p class='num'>%score% Gems collected!</p>";
+      var formattedScore = scoreHTML.replace("%score%", this.count);
+    }
+    $(".num").remove();
+    //////////  Update score  //////////
+    $(".score").append(formattedScore);
+  }
+}
+
+Gem.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.gemImg), this.x, this.y);
+}
+
+// Keep track of player lives 
+var Life = function() {
+  this.life = 5;
+}
+
+Life.prototype.decrease = function() {
+  if (this.life > 0) {
+    this.life -= 1;
+  }
+  else
+    // replace with modal
+    alert("Game Over!");
+}
+
+////////////// Playing with Guns and Land Mines ///////////////
+
+var bullets = [];
+var explosions = [];
+var isGunFound = true;
+var lastFire = Date.now();
+
+var Bullet = function() {
+  this.bImg = 'img/bullet.png';
+  this.speed = 500;
+  this.shoot = function(){
+      if (player.ctlKey === 'space' && isGunFound && Date.now() - lastFire > 100){
+        var x = player.x;  // Figure out middle of player
+        var y = player.y;  // Figure out middle of player
+
+        bullets.push( { pos: [x, y],
+                        dir: 'up',  // What does 'forward' mean?
+                        sprite: this.bImg });
+        lastFire = Date.now();
+        }
+    }
+}
+
+Bullet.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.bImg), this.x, this.y);
+}
+
+Bullet.prototype.update = function(dt) {
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x = this.x + (this.speed * dt);
+  if (this.x > 960) {
+    this.x = -100;
+    this.y = this.y + 83;
+    if (this.y > 226) {
+      this.y = 60;
+    }
+  }
+
+  if (this.x > -50 && this.x < 50) {
+    this.tileX = 0;
+  } else if (this.x > 50 && this.x < 150) {
+    this.tileX = 101;
+  } else if (this.x > 150 && this.x < 250) {
+    this.tileX = 202;
+  } else if (this.x > 250 && this.x < 350) {
+    this.tileX = 303;
+  } else if (this.x > 350 && this.x < 450) {
+    this.tileX = 404;
+  } else if (this.x > 450 && this.x < 550) {
+    this.tileX = 505;
+  } else if (this.x > 550 && this.x < 650) {
+    this.tileX = 606;
+  } else if (this.x > 650 && this.x < 750) {
+    this.tileX = 707;
+  } else if (this.x > 750 && this.x < 850) {
+    this.tileX = 808;
+  } else if (this.x > 850) {
+    this.tileX = 1;
+  }
+
+  if (enemyA.x === this.tileX && enemyA.y === this.y) {
+    enemyA.reset();
+  }
+}
+
+///////////// End Playing with Guns and Land Mines /////////////
+
+// Now instantiate your objects.
+var enemyA = new Enemy();
+var enemyB = new Enemy();
+var enemyC = new Enemy();
+var enemyD = new Enemy();
+var allEnemies = [enemyA, enemyB, enemyC, enemyD];
+
+var player = new Player();
+var gem = new Gem();
+var gameLife = new Life();
+var bullet = new Bullet();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    32: 'space'
+  };
+  player.handleInput(allowedKeys[e.keyCode]);
+});
+// This listens for key presses and disables default scroll actions.
+document.addEventListener('keydown', function(e) {
+  if ([37, 38, 39, 40, 32].indexOf(e.keyCode) > -1) {
+    e.preventDefault();
+  }  
+}, false);
---
+++ js/engine.js
@@ -0,0 +1,144 @@
+var Engine = (function(global) {
+  var doc = global.document,
+    win = global.window,
+    canvas = doc.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    patterns = {},
+    lastTime;
+
+  canvas.width = 909;
+  canvas.height = 610;
+  doc.getElementById('c').appendChild(canvas);
+
+  function main() {
+    var now = Date.now(),
+      dt = (now - lastTime) / 1000.0;
+
+    update(dt);
+    render();
+
+    lastTime = now;
+    win.requestAnimationFrame(main);
+  };
+
+  function init() {
+
+    lastTime = Date.now();
+    main();
+  }
+
+  function update(dt) {
+    updateEntities(dt);
+    // checkCollisions();
+  }
+
+  function updateEntities(dt) {
+    allEnemies.forEach(function(enemy) {
+      enemy.update(dt);
+    });
+    player.update();
+    gem.update();
+  }
+
+  function render() {
+    var rowImages = [
+        'images/water-block.png',
+        'images/stone-block.png',
+        'images/stone-block.png',
+        'images/stone-block.png',
+        'images/grass-block.png',
+        'images/grass-block.png'
+      ],
+      numRows = 6,
+      numCols = 9,
+      row, col;
+
+    for (row = 0; row < numRows; row++) {
+      for (col = 0; col < numCols; col++) {
+        ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+      }
+    }
+
+    renderEntities();
+  }
+
+  function renderEntities() {
+    allEnemies.forEach(function(enemy) {
+      enemy.render();
+    });
+    player.render();
+    gem.render();
+  }
+
+  Resources.load([
+    'images/stone-block.png',
+    'images/water-block.png',
+    'images/grass-block.png',
+    'images/enemy-bug.png',
+    'images/char-princess-girl.png',
+    'images/gemOrange.png',
+    'images/gemBlue.png',
+    'images/gemGreen.png',
+    'images/sprites.png'  // Load explosion and bullets
+  ]);
+  Resources.onReady(init);
+
+  global.ctx = ctx;
+})(this);
+
+// Animation helper  from https://github.com/jlongster/canvas-game-bootstrap/blob/a878158f39a91b19725f726675c752683c9e1c08/js/sprite.js
+
+(function() {
+    function Sprite(url, pos, size, speed, frames, dir, once) {
+        this.pos = pos;
+        this.size = size;
+        this.speed = typeof speed === 'number' ? speed : 0;
+        this.frames = frames;
+        this._index = 0;
+        this.url = url;
+        this.dir = dir || 'horizontal';
+        this.once = once;
+    };
+
+    Sprite.prototype = {
+        update: function(dt) {
+            this._index += this.speed*dt;
+        },
+
+        render: function(ctx) {
+            var frame;
+
+            if(this.speed > 0) {
+                var max = this.frames.length;
+                var idx = Math.floor(this._index);
+                frame = this.frames[idx % max];
+
+                if(this.once && idx >= max) {
+                    this.done = true;
+                    return;
+                }
+            }
+            else {
+                frame = 0;
+            }
+
+            var x = this.pos[0];
+            var y = this.pos[1];
+
+            if(this.dir == 'vertical') {
+                y += frame * this.size[1];
+            }
+            else {
+                x += frame * this.size[0];
+            }
+
+            ctx.drawImage(resources.get(this.url),
+                          x, y,
+                          this.size[0], this.size[1],
+                          0, 0,
+                          this.size[0], this.size[1]);
+        }
+    };
+
+    window.Sprite = Sprite;
+})();
